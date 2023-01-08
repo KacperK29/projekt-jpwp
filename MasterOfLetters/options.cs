cd4090fc5a41680d55759c803e5fe6f2e73ec4b1
KacperK29 <116479433+KacperK29@users.noreply.github.com>
2023-01-08 08:53:45 +0100

    Add files via upload

---
+++ MasterOfLetters/options.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterOfLetters
+{
+    public partial class options : Form
+    {
+        /// <summary>
+        /// Temporary variable.
+        /// </summary>
+        public int r;
+        public int time;
+        public static options instance;
+        public options()
+        {
+            InitializeComponent();
+            instance = this;
+            Get_Values();
+
+        }
+
+        /// <summary>
+        /// Closes this window when clicked.
+        /// </summary>
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// Sets the number of simultaneously displayed letters to 1.
+        /// </summary>
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            r = 1;
+            Form1.instance.n = r;
+            Form1.instance.starter(r);
+
+            Form1.instance.pause();
+        }
+        /// <summary>
+        /// Sets the number of simultaneously displayed letters to 2.
+        /// </summary>
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            r = 2;
+            Form1.instance.n = r;
+            Form1.instance.starter(r);
+
+            Form1.instance.pause();
+        }
+        /// <summary>
+        /// Sets the number of simultaneously displayed letters to 3.
+        /// </summary>
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            r = 3;
+            Form1.instance.n = r;
+            Form1.instance.starter(r);
+
+            Form1.instance.pause();
+        }
+        /// <summary>
+        /// Changes the value of target in Form1.cs
+        /// </summary>
+
+        private void numericUpDownTarget_ValueChanged(object sender, EventArgs e)
+        {
+            Form1.instance.target = (int)(numericUpDownTarget.Value);
+            Form1.instance.starter(r);
+            Form1.instance.pause();
+        }
+        /// <summary>
+        /// Changes the value of seconds in Form1.cs
+        /// </summary>
+
+        private void numericUpDownTime_ValueChanged(object sender, EventArgs e)
+        {
+            Form1.instance.seconds = (int)(numericUpDownTime.Value);
+            Form1.instance.starter(r);
+            Form1.instance.pause();
+        }
+        /// <summary>
+        /// Copies values of seconds and target form Form1 and sets them to proper NumericUpDowns.
+        /// </summary>
+        private void Get_Values()
+        {
+            numericUpDownTime.Value = Form1.instance.seconds;
+            numericUpDownTarget.Value = Form1.instance.target;
+        }
+
+
+        /// <summary>
+        /// Enables sound effects.
+        /// </summary>
+
+        private void radioButtonEnabled_CheckedChanged(object sender, EventArgs e)
+        {
+            Form1.instance.sound_enabled = true;
+        }
+        /// <summary>
+        /// Disables sound effects.
+        /// </summary>
+
+        private void radioButtonDisabled_CheckedChanged(object sender, EventArgs e)
+        {
+            Form1.instance.sound_enabled = false;
+        }
+    }
+}
---
+++ MasterOfLetters/scoreboard.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace MasterOfLetters
+{
+    partial class scoreboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelText = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.labelText);
+            this.panel1.Controls.Add(this.buttonClose);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-2, -5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(802, 555);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelText
+            // 
+            this.labelText.BackColor = System.Drawing.SystemColors.Control;
+            this.labelText.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelText.Location = new System.Drawing.Point(46, 82);
+            this.labelText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(710, 404);
+            this.labelText.TabIndex = 2;
+            this.labelText.Text = "l";
+            this.labelText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(286, 501);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(225, 30);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "CLOSE";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(233, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SCOREBOARD";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 547);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "scoreboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scoreboard";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelText;
+    }
+}
